58edf3ef593d77534a4f2437aca86cda20fcf261
robodanill <khalikhalik@mail.ru>
2024-05-05 23:44:03 +0300

    third task

--- project_tracker/quality_control/views.py
+++ project_tracker/quality_control/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.urls import reverse
 
-# Create your views here.
+def index(request):
+    bug_reports_url = reverse('quality_control:bug_list')
+    feature_request_url = reverse('quality_control:feature_list')
+    html = f"<h1>Система контроля качества</h1><a href='{bug_reports_url}'><h2>Список всех багов</a></h2><h2><a href='{feature_request_url}'>Запросы на улучшение</a></h2>"
+    return HttpResponse(html)
+
+def bug_list(request):
+    return HttpResponse("Список всех багов.")
+
+def feature_list(request):
+    return HttpResponse("Запросы на улучшение.")
